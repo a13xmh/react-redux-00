dd7e21ae28d276f018f69a291fe9e0f8a9b88fe1
Alejandro <alejandro.martinez@dedge.global>
2018-08-14 13:18:40 -0500

    se agrega mapStateToProps, para obtener la informacion del store

--- src/components/containers/TrendingContainer.js
+++ src/components/containers/TrendingContainer.js
@@ -13,14 +13,34 @@ class TrendingContainer extends Component {
     }
 
     componentDidMount(){
-        this.props.actionTest();
+        this.props.actionTest({data:"loremp ipsum"});
     }
 
     render(){
-    return(<div>TrendingContainer (Componente React)</div>)  
+    return(
+        <div>
+            <div>Informacion proveniente del Store de Redux:</div>
+            <br />
+            <div><strong>{JSON.stringify(this.props.trending)}</strong></div>
+            <br />
+            <div>Flujo:</div>
+            <img width="500"  src="https://camo.githubusercontent.com/5aba89b6daab934631adffc1f301d17bb273268b/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6d656469612d702e736c69642e65732f75706c6f6164732f3336343831322f696d616765732f323438343535322f415243482d5265647578322d7265616c2e676966" />           
+        </div>)  
     }
 
 };
+
+/*
+Se obtiene la información del store (del reducer trending), en este punto aun no se utilizan selectores
+*/
+function mapStateToProps(state) {
+    let { trending } = state;
+
+    return {
+      trending
+    };
+  }
+
 
   /*
   Inyecta los action creators a nuestro container, via props 
@@ -37,6 +57,6 @@ class TrendingContainer extends Component {
 
 
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
   )(TrendingContainer);
