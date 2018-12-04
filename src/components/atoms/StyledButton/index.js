17d556b2f91e84159acceca146e731f3bf04cbb1
Alejandro <a13xmh@gmail.com>
2018-12-04 16:21:33 -0600

    Se agrega Styled-Components y un atomo basico

---
+++ src/components/atoms/StyledButton/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled, { css } from 'styled-components'
+
+const StyledButton = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+
+  ${props => props.primary && css`
+    background: palevioletred;
+    color: white;
+  `}
+`;
+
+
+export default StyledButton;
--- src/components/containers/TrendingContainer.js
+++ src/components/containers/TrendingContainer.js
@@ -1,7 +1,12 @@
 import React from "react";
+import StyledButton from "../atoms/StyledButton"
 
 const TrendingContainer = () => (
-    <div>TrendingContainer</div>
+    <div>
+        <h1>Random container</h1>
+        <StyledButton>Normal Button</StyledButton>
+        <StyledButton primary>Primary Button</StyledButton>
+    </div>
 );
 
 export default TrendingContainer;
